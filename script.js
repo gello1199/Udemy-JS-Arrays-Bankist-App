0e17414c3ab149117a2008a84170d21a5c4dd768
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-04-05 22:16:27 -0400

    Simple array methods notes

--- script.js
+++ script.js
@@ -77,7 +77,36 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // Simple Array Methods
 
+// let arr = ['a', 'b', 'c', 'd', 'e']
+
+// SLICE - doesn't mutate original array
+// console.log(arr.slice(2))
+// console.log(arr.slice(2,4))
+// console.log(arr.slice(-2))
+// console.log(arr.slice(-1))
+// console.log(arr.slice(1, -2))
+// console.log(arr.slice())
+// console.log(arr.slice([...arr]))
+// personal preference to use concat or spread operator
+
+// SPLICE - mutates the original array
+// console.log(arr.splice(2))
+// arr.splice(-1)
+// console.log(arr)
+// arr.splice(1, 2)
+// console.log(arr)
+
+// REVERSE - mutates original array
 let arr = ['a', 'b', 'c', 'd', 'e']
+const arr2 = ['j', 'i', 'h', 'g', 'f']
+console.log(arr2.reverse())
+console.log(arr2)
 
-console.log(arr.slice(2))
+// CONCAT - Doesn't mutate original array
+const letters = arr.concat(arr2)
+console.log(letters)
+console.log(...arr, ...arr2)
+// personal preference to use concat or spread operator
 
+// JOIN 
+console.log(letters.join(' - '))
