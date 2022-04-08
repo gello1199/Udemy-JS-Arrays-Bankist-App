01160ad58ebe7452d4d8fe6d2c8d00100693cd84
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-04-07 22:32:47 -0400

    Started Bankist app, creating displaymovements function

--- script.js
+++ script.js
@@ -61,6 +61,22 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function(movements) {
+  movements.forEach(function(mov, i) {
+    
+
+
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--deposit">${i + 1}</div>
+        <div class="movements__value">${mov}</div>
+      </div>
+    `
+
+  })
+}
+displayMovements(account1.movements)
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -151,20 +167,21 @@ const inputClosePin = document.querySelector('.form__input--pin');
 // forEach with Maps and Sets
 
 // MAP
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
-currencies.forEach(function(value, key, map) {
-  console.log(`${key}: ${value}`)
-})
+// currencies.forEach(function(value, key, map) {
+//   console.log(`${key}: ${value}`)
+// })
 
-// SET
-const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR'])
-console.log(currenciesUnique)
+// // SET
+// const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR'])
+// console.log(currenciesUnique)
 
-currenciesUnique.forEach(function(value, _, map) {
-  console.log(`${value}: ${value}`)
-})
+// currenciesUnique.forEach(function(value, _, map) {
+//   console.log(`${value}: ${value}`)
+// })
+
