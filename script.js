0476f0a75e4d5818daccda0e17b881d902f1863a
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-04-14 23:41:10 -0400

    the filer method notes

--- script.js
+++ script.js
@@ -93,9 +93,22 @@ console.log(accounts)
 
 // LECTURES
 
+// The Filter Method
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const deposits = movements.filter(mov => mov > 0)
+console.log(movements)
+console.log(deposits)
+
+const withdrawal = movements.filter(mov => mov < 0) 
+console.log(withdrawal)
+
+// const depositsFor = []
+// for(const mov of movements) if(mov > 0) depositsFor.push(mov)
+// console.log(depositsFor)
+
 // Computing Usernames
 // See createUsernames function above
-
 
 // The map Method
 
