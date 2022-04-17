dc26a1b0224f33dfed36c9fc97f382b95b78c2e7
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-04-16 23:28:39 -0400

    coding challenge 2 started

--- script.js
+++ script.js
@@ -99,8 +99,34 @@ createUsernames(accounts)
 
 // LECTURES
 
+// Coding Challenge #2
+
+/* 
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
+
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+
+1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4.
+2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
+3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
+4. Run the function for both test datasets
+
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+const calcAverageHumanAge = function(ages) {
+  const humanAges = ages.map(age => age <= 2 ? 2 * age : 16 + age * 4)
+  console.log(humanAges)
+}
+
+calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3])
+calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4])
+
 // The reduce method
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // accumulator -> SNOWBALL
 // const balance = movements.reduce(function(acc, curr, i, arr) {
@@ -118,12 +144,12 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // Reduce: Get maximum value of movements array
 
-const max = movements.reduce((acc, mov) => {
-  if(acc > mov)
-  return acc
-  else return mov
-}, movements[0])
-console.log(max)
+// const max = movements.reduce((acc, mov) => {
+//   if(acc > mov)
+//   return acc
+//   else return mov
+// }, movements[0])
+// console.log(max)
 
 // The Filter Method
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
