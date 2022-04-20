3fe7f2d6521f62d82fbaeffc00b065f85c81e706
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-04-20 11:25:55 -0400

    the find method notes

--- script.js
+++ script.js
@@ -121,6 +121,20 @@ createUsernames(accounts)
 
 // LECTURES
 
+// The Find Method
+// find only returns the first element that satisfies the condition
+// filer returns a new array
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const firstWithdrawal = movements.find(mov => mov < 0)
+console.log(firstWithdrawal)
+
+console.log(accounts)
+
+const account = accounts.find(acc => acc.owner === 'Jessica Davis')
+console.log(account)
+
+
 // Coding Challenge #3
 
 /* 
@@ -132,14 +146,14 @@ TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 GOOD LUCK 😀
 */
 
-const calcAverageHumanAge = ages => ages
-.map(age => age <= 2 ? 2 * age : 16 + age * 4)
-.filter(age => age > 18)
-.reduce((acc, age, i, arr) => acc + age / arr.length, 0)
+// const calcAverageHumanAge = ages => ages
+// .map(age => age <= 2 ? 2 * age : 16 + age * 4)
+// .filter(age => age > 18)
+// .reduce((acc, age, i, arr) => acc + age / arr.length, 0)
 
-const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3])
-const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4])
-console.log(avg1, avg2)
+// const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3])
+// const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4])
+// console.log(avg1, avg2)
 
 // The magic of chaining methods
 
