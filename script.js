d08ca24d20cc30a7c20efd22b8026d7f0eea804a
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-04-20 23:13:23 -0400

    Implementing Login

--- script.js
+++ script.js
@@ -76,35 +76,32 @@ const displayMovements = function(movements) {
     containerMovements.insertAdjacentHTML('afterbegin', html)
   })
 }
-displayMovements(account1.movements)
 
 const calcDisplayBalance = function(movements) {
   const balance = movements.reduce((acc, mov) => acc + mov, 0)
   labelBalance.textContent = `${balance} €`
 }
-calcDisplayBalance(account1.movements)
 
-const calcDisplaySummary = function(movements) {
-  const incomes = movements
+const calcDisplaySummary = function(acc) {
+  const incomes = acc.movements
   .filter(mov => mov > 0)
   .reduce((acc, mov) => acc + mov, 0)
   labelSumIn.textContent = `${incomes} €`
 
-  const out = movements
+  const out = acc.movements
   .filter(mov => mov < 0)
   .reduce((acc, mov) => acc + mov, 0)
   labelSumOut.textContent = `${Math.abs(out)} €` 
 
-  const interest = movements
+  const interest = acc.movements
   .filter(mov => mov > 0)
-  .map(deposit => deposit * 1.2/100)
+  .map(deposit => deposit * acc.interestRate/100)
   .filter((int, i, arr) => {
     return int >= 1
   })
   .reduce((acc, int) => acc + int, 0)
   labelSumInterest.textContent = `${interest} €`
 }
-calcDisplaySummary(account1.movements)
 
 const createUsernames = function(accs) {
   accs.forEach(acc => {
@@ -116,23 +113,57 @@ const createUsernames = function(accs) {
 createUsernames(accounts)
 // console.log(accounts)
 
+// Event handler
+let currentAccount;
+
+btnLogin.addEventListener('click', function(e) {
+  e.preventDefault()
+ 
+  currentAccount = accounts.find(acc => acc.username === inputLoginUsername.value)
+  console.log(currentAccount)
+
+  // optional chaining. only look for pin if currentaccount exists
+  if(currentAccount?.pin === Number(inputLoginPin.value)) {
+    // Display UI and welcome message
+    labelWelcome.textContent = `Welcome back, ${currentAccount.owner.split(' ')[0]}`
+    containerApp.style.opacity = 100
+
+    // Clear input fields 
+    inputLoginUsername.value = inputLoginPin.value = ''
+    inputLoginPin.blur()
+
+    // Display movements
+    displayMovements(currentAccount.movements)
+
+    // Display balance
+    calcDisplayBalance(currentAccount.movements)
+
+    // Display summary
+    calcDisplaySummary(currentAccount)
+  }
+})
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 
 // LECTURES
 
+// Implementing Login
+
+
+
 // The Find Method
 // find only returns the first element that satisfies the condition
 // filer returns a new array
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-const firstWithdrawal = movements.find(mov => mov < 0)
-console.log(firstWithdrawal)
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const firstWithdrawal = movements.find(mov => mov < 0)
+// console.log(firstWithdrawal)
 
-console.log(accounts)
+// console.log(accounts)
 
-const account = accounts.find(acc => acc.owner === 'Jessica Davis')
-console.log(account)
+// const account = accounts.find(acc => acc.owner === 'Jessica Davis')
+// console.log(account)
 
 
 // Coding Challenge #3
