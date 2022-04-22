36e5d774fc5a8d34854ffaf8b8b629027f1dca7b
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-04-21 22:30:25 -0400

    Implementing Transfers notes

--- script.js
+++ script.js
@@ -77,9 +77,9 @@ const displayMovements = function(movements) {
   })
 }
 
-const calcDisplayBalance = function(movements) {
-  const balance = movements.reduce((acc, mov) => acc + mov, 0)
-  labelBalance.textContent = `${balance} €`
+const calcDisplayBalance = function(acc) {
+  acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0)
+  labelBalance.textContent = `${acc.balance} €`
 }
 
 const calcDisplaySummary = function(acc) {
@@ -113,6 +113,17 @@ const createUsernames = function(accs) {
 createUsernames(accounts)
 // console.log(accounts)
 
+const updateUI = function(acc) {
+     // Display movements
+     displayMovements(acc.movements)
+
+     // Display balance
+     calcDisplayBalance(acc)
+ 
+     // Display summary
+     calcDisplaySummary(acc)
+}
+
 // Event handler
 let currentAccount;
 
@@ -132,14 +143,25 @@ btnLogin.addEventListener('click', function(e) {
     inputLoginUsername.value = inputLoginPin.value = ''
     inputLoginPin.blur()
 
-    // Display movements
-    displayMovements(currentAccount.movements)
+    // Update UI
+    updateUI(currentAccount)
+  }
+})
 
-    // Display balance
-    calcDisplayBalance(currentAccount.movements)
+btnTransfer.addEventListener('click', function(e) {
+  e.preventDefault()
+  const amount = Number(inputTransferAmount.value)
+  const receiverAcc = accounts.find(acc => acc.username === inputTransferTo.value) 
 
-    // Display summary
-    calcDisplaySummary(currentAccount)
+  inputTransferAmount.value = inputTransferTo.value = ''
+
+  if(amount > 0 && receiverAcc && currentAccount.balance >= amount && receiverAcc?.username !== currentAccount.username) {
+    // Doing the transfer
+    currentAccount.movements.push(-amount)
+    receiverAcc.movements.push(amount)
+
+     // Update UI
+     updateUI(currentAccount)
   }
 })
 
@@ -147,10 +169,6 @@ btnLogin.addEventListener('click', function(e) {
 /////////////////////////////////////////////////
 
 // LECTURES
-
-// Implementing Login
-
-
 
 // The Find Method
 // find only returns the first element that satisfies the condition
