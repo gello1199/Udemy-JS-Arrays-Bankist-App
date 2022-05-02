00ed5da6e30d13734cf5e88ebe70dc086379eee6
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-05-02 00:30:20 -0400

    reduce method with multiple parameters

--- script.js
+++ script.js
@@ -242,7 +242,15 @@ console.log(++a)
 console.log(a)
 
 // 3.
+const {deposits, withdrawls} = accounts
+.flatMap(acc => acc.movements)
+.reduce((sums, cur) => {
+  // cur > 0 ? sums.deposits += cur : sums.withdrawls += cur
+  sums[cur > 0 ? 'deposits' : 'withdrawls'] += cur
+  return sums
+}, {deposits: 0, withdrawls: 0})
 
+console.log(deposits, withdrawls)
 
 // More ways of creating arrays
 
