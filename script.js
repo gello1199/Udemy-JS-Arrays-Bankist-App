fa821e1b1635b191c03f968edaeb42d2cfe0c0cf
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2022-05-07 13:40:24 -0400

    Array methods practice refactored 4th example

--- script.js
+++ script.js
@@ -286,14 +286,17 @@ console.log(deposits, withdrawls)
 // this is a nice title => This Is a Nice Title
 
 const convertTitleCase = function(title) {
+  const capitalize = str => str[0].toUpperCase() + str.slice(1)
+
   const exceptions = ['a', 'an', 'and', 'the', 'but', 'or', 'on', 'in', 'with']
 
   const titleCase = title
   .toLowerCase()
   .split(' ')
   .map(word => 
-    exceptions.includes(word) ? word : word[0].toUpperCase() + word.slice(1)).join(' ')
-  return titleCase
+    (exceptions.includes(word) ? word : capitalize(word)))
+    .join(' ')
+  return capitalize(titleCase)
 
 }
 console.log(convertTitleCase('this is a nice title'))
